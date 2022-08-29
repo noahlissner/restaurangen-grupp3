61fdf67ab5fe091095fd266e95b520487f247a72
Simon Sandahl <sandahl.simon@hotmail.se>
2022-08-29 20:21:44 +0200

    more setup + start of nav menu

---
+++ frontend/src/components/HamburgerNav.tsx
@@ -0,0 +1,3 @@
+export const HamburgerNav = () => {
+  return <>Hamham</>;
+};
--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,10 +1,12 @@
 import "../styles/Layout.scss";
 import { Outlet } from "react-router-dom";
+import { HamburgerNav } from "./HamburgerNav";
 
 export const Layout = () => {
   return (
     <div className="layout-container">
-      LAYOUT WORKS!
+      <div>LAYOUT WORKS!</div>
+      <HamburgerNav />
       <div>
         <Outlet></Outlet>
       </div>
