603cea085220366221c45e8cd496f1413cded091
Simon Sandahl <sandahl.simon@hotmail.se>
2022-08-31 20:57:42 +0200

    start of photo carousel

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -1,3 +1,23 @@
+import "../../styles/Home.scss";
+import Carousel from "nuka-carousel";
+import kebabOne from "../../assets/kebab-1.jpeg";
+import kebabTwo from "../../assets/kebab-2.jpeg";
+import kebabThree from "../../assets/kebab-3.jpeg";
+
 export const Home = () => {
-  return <>Home Works!</>;
+  return (
+    <>
+      <Carousel dragging={true} wrapAround={true}>
+        <div className="img-container">
+          <img src={kebabOne} />
+        </div>
+        <div className="img-container">
+          <img src={kebabTwo} />
+        </div>
+        <div className="img-container">
+          <img src={kebabThree} />
+        </div>
+      </Carousel>
+    </>
+  );
 };
