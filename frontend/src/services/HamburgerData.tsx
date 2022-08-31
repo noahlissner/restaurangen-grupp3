341df07e56765bf2f1a0d7ee436c3c89a642f0b1
Simon Sandahl <sandahl.simon@hotmail.se>
2022-08-31 13:43:05 +0200

    finished nav menu for mobile

---
+++ frontend/src/services/HamburgerData.tsx
@@ -0,0 +1,22 @@
+export const HamburgerData = [
+  {
+    title: "Home",
+    path: "/",
+    cName: "nav-text",
+  },
+  {
+    title: "Have a seat",
+    path: "/booking",
+    cName: "nav-text",
+  },
+  {
+    title: "Contact",
+    path: "#contact",
+    cName: "nav-text",
+  },
+  {
+    title: "Socials",
+    path: "#footer",
+    cName: "nav-text",
+  },
+];
