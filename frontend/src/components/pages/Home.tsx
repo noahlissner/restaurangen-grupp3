b439a80be38a84092cb371d7fa1582c7d8d015f7
Simon Sandahl <sandahl.simon@hotmail.se>
2022-08-31 22:27:16 +0200

    start of contact page

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -4,6 +4,7 @@ import kebabOne from "../../assets/kebab-1.jpeg";
 import kebabTwo from "../../assets/kebab-2.jpeg";
 import kebabThree from "../../assets/kebab-3.jpeg";
 import grabbarna from "../../assets/doners-soner.jpeg";
+import geolocation from "../../assets/geolocation.png";
 
 export const Home = () => {
   return (
@@ -24,7 +25,7 @@ export const Home = () => {
           <img src={grabbarna} alt="bild på ägarna" />
         </div>
         <div className="about-text-container">
-          <h3>ABOUT DöNERS SöNER</h3>
+          <h1>ABOUT DöNERS SöNER</h1>
           <p>
             Allt startade från en dröm. Sen här kommer massa filler text där jag
             inte har en aning om vad jag ska skriver tbh. Så vi får ändra den
@@ -37,6 +38,32 @@ export const Home = () => {
             Ett till paragraf? Nån av dem är rätt. Så här är jag igen och
             skriver mer och mer. Nu räcker de nog.
           </p>
+        </div>
+      </div>
+      <div id="contact">
+        <div className="contact-text-container">
+          <h1>CONTACT</h1>
+          <div className="contact-info-container">
+            <h2>Stockholm</h2>
+            <div>
+              <span className="lg-text">Ettgatunamn 177</span>
+            </div>
+            <div>
+              <span className="sm-text">116 42, Stockholm</span>
+            </div>
+            <div>
+              <span className="lg-text">08 - 12 34 56</span>
+            </div>
+            <div>
+              <span className="lg-text">Mailadresstilloss@donerssoner.com</span>
+            </div>
+          </div>
+        </div>
+        <div className="contact-img-container">
+          <img
+            src={geolocation}
+            alt="bild på karta med adress där resturangen ligger"
+          />
         </div>
       </div>
     </>
