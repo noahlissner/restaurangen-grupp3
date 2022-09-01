74849a80db149a6733071b1589e4a22fba165300
Simon Sandahl <sandahl.simon@hotmail.se>
2022-09-01 12:42:46 +0200

    added footer content

--- frontend/src/components/pages/Home.tsx
+++ frontend/src/components/pages/Home.tsx
@@ -5,6 +5,7 @@ import kebabTwo from "../../assets/kebab-2.jpeg";
 import kebabThree from "../../assets/kebab-3.jpeg";
 import grabbarna from "../../assets/doners-soner.jpeg";
 import geolocation from "../../assets/geolocation.png";
+// import logo_placeholder from "../../assets/logo-placeholder.png";
 
 export const Home = () => {
   return (
@@ -64,6 +65,31 @@ export const Home = () => {
             src={geolocation}
             alt="bild på karta med adress där resturangen ligger"
           />
+        </div>
+      </div>
+      <div id="footer">
+        <div className="footer-container">
+          <div className="footer-logo-container">
+            <img src={""} alt="" />
+          </div>
+          <div className="footer-find-us">
+            <h2>FIND US</h2>
+            <div>
+              <span className="lg-text">Ettgatunamn 177</span>
+            </div>
+            <div>
+              <span className="sm-text">116 42, Stockholm</span>
+            </div>
+          </div>
+          <div className="footer-contact-us">
+            <h2>CONTACT US</h2>
+            <div>
+              <span className="lg-text">08 - 12 34 56</span>
+            </div>
+            <div>
+              <span className="lg-text">Mailadresstilloss@donerssoner.com</span>
+            </div>
+          </div>
         </div>
       </div>
     </>
