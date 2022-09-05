29a0a698f4ff7e6eb834f99752aa873ba6f73b93
Simon Sandahl <sandahl.simon@hotmail.se>
2022-09-05 15:13:30 +0200

    added navbar for dekstop

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,11 +1,13 @@
 import "../styles/Layout.scss";
 import { Outlet } from "react-router-dom";
 import { HamburgerNav } from "./HamburgerNav";
+import { DesktopNav } from "./DesktopNav";
 
 export const Layout = () => {
   return (
     <div className="layout-container">
       <HamburgerNav />
+      <DesktopNav />
       <div>
         <Outlet></Outlet>
       </div>
