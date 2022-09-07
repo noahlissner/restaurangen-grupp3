89a494a9d5a3329d7bf93b884fb81ae4c62c747a
Simon Sandahl <sandahl.simon@hotmail.se>
2022-09-07 18:29:18 +0200

    added some functionality to calendar

--- frontend/src/components/pages/Booking.tsx
+++ frontend/src/components/pages/Booking.tsx
@@ -1,12 +1,23 @@
 import "../../styles/Booking.scss";
 import { Calendar } from "react-calendar";
 import { format, startOfDay, startOfToday } from "date-fns";
+import { useState } from "react";
 
 export const Booking = () => {
   let today = startOfToday();
+  const [date, setDate] = useState(today);
+
+  console.log(format(date, "EEEE do MMMM yyyy"));
   return (
     <div className="booking-wrapper">
-      <Calendar next2Label={null} prev2Label={null} />
+      <Calendar
+        onChange={setDate}
+        minDate={today}
+        navigationLabel={({ date }) => `${format(date, "MMMM")}`}
+        next2Label={null}
+        prev2Label={null}
+        value={date}
+      />
       <div className="booking-btns">
         <button className="booking-btns-cancel">Cancel</button>
         <button className="booking-btns-pick">Pick Date</button>
