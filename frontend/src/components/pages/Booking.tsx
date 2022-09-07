38be26c1b929042ee421d7fac17bc4ef5a15432d
Simon Sandahl <sandahl.simon@hotmail.se>
2022-09-07 17:26:19 +0200

    bookings calendar layout. With some new packages

--- frontend/src/components/pages/Booking.tsx
+++ frontend/src/components/pages/Booking.tsx
@@ -1,9 +1,16 @@
 import "../../styles/Booking.scss";
+import { Calendar } from "react-calendar";
+import { format, startOfDay, startOfToday } from "date-fns";
 
 export const Booking = () => {
+  let today = startOfToday();
   return (
     <div className="booking-wrapper">
-      <h3>Booking Works!</h3>
+      <Calendar next2Label={null} prev2Label={null} />
+      <div className="booking-btns">
+        <button className="booking-btns-cancel">Cancel</button>
+        <button className="booking-btns-pick">Pick Date</button>
+      </div>
     </div>
   );
 };
