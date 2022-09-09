a17e014d17446d54e21b8227e46b7336f007c7a1
noahlissner <noah.lissner@gmail.com>
2022-09-09 14:37:54 +0200

    feat: Collect all the data and send post request to backend and create booking

--- frontend/src/containers/BookingScreenTwo.tsx
+++ frontend/src/containers/BookingScreenTwo.tsx
@@ -1,4 +1,5 @@
-import React, { Dispatch, FormEvent, SetStateAction } from "react";
+import React, { Dispatch, FormEvent, SetStateAction, useState } from "react";
+import BookingRadio from "../components/BookingRadio";
 
 interface IProps {
   time: Dispatch<SetStateAction<string>>;
@@ -9,6 +10,19 @@ interface IProps {
 }
 
 const BookingScreenTwo = ({ time, name, email, phone, onSubmit }: IProps) => {
+  const [timeChecked, setTimeChecked] = useState("");
+
+  const handleRadioChange = (e: any) => {
+    setTimeChecked(e.target.value);
+    time(e.target.value);
+  };
+
+  const handleInputChange = (e: any) => {
+    if (e.target.name === "name") name(e.target.value);
+    if (e.target.name === "email") email(e.target.value);
+    if (e.target.name === "phone") phone(e.target.value);
+  };
+
   return (
     <section className="bookingTwo">
       <div className="bookingTwo__container">
@@ -22,21 +36,52 @@ const BookingScreenTwo = ({ time, name, email, phone, onSubmit }: IProps) => {
             <div>
               <input
                 type="text"
+                name="name"
                 placeholder="Namn"
                 className="bookingTwo-text-input"
+                onChange={handleInputChange}
               />
               <input
                 type="tel"
+                name="phone"
                 placeholder="Telefon"
                 className="bookingTwo-text-input"
+                onChange={handleInputChange}
               />
             </div>
             <input
               type="email"
+              name="email"
               placeholder="Email"
               className="bookingTwo-text-input"
+              onChange={handleInputChange}
             />
           </div>
+          <div className="bookingTwo__radio-container">
+            <h4>Tid</h4>
+            <div>
+              <BookingRadio
+                value="18"
+                checked={timeChecked}
+                onChange={handleRadioChange}
+                title="18:00"
+                name="time"
+              />
+              <BookingRadio
+                value="21"
+                checked={timeChecked}
+                onChange={handleRadioChange}
+                title="21:00"
+                name="time"
+              />
+            </div>
+          </div>
+          <button
+            className="bookingTwo__container__form-wrapper__submit"
+            type="submit"
+          >
+            Boka Bord
+          </button>
         </form>
       </div>
     </section>
